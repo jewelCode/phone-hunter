b8d2630bb8f77a66f9de8a99c0b4a1a1fdbe3595
jewelCode <jewelbarua101@gmail.com>
2022-03-02 09:11:57 +0600

    slice of 20 devices completed

--- smart-finder.js
+++ smart-finder.js
@@ -3,27 +3,32 @@ const searchInput = () => {
     const url = `https://openapi.programming-hero.com/api/phones?search=${inputPhone}`
     fetch(url)
     .then(response => response.json())
-    .then(data => displayPhone(data.data))
+    .then(data => displayPhone(data.data.slice(0, 20)))
 }
 
-const displayPhone = (phone) => {
+const displayPhone = (phones) => {
     const phoneCard = document.getElementById("phone-card");
-    for(const ph of phone){
-        console.log(ph);
+    console.log(phones)
+    for(const phone of phones){
+        // console.log(phone);
         const div = document.createElement('div');
         div.classList.add("col-md-4");
         div.innerHTML = `
         
           <div class="border text-center p-3">
-                <div class="mt-3"><img src=${ph.image}></div>
-                <h5>${ph.brand}</h5>
-                <h5>${ph.phone_name}</h5>
-                <button class="btn btn-primary">See More</button>
+                <div class="mt-3"><img src=${phone.image}></div>
+                <h5>${phone.brand}</h5>
+                <h5>${phone.phone_name}</h5>
+                <button onClick="displayPhoneDetails(${phone.slug})" class="btn btn-primary">See More</button>
           </div>
-          
         `
         phoneCard.appendChild(div);
         
     }
+}
+
+const displayPhoneDetails = slug =>{
+    console.log(slug);
+    
 }
 
